0c39b21e81ed117b63c379a3353aafc4b9ac049e
maxwen <maxwen.me@gmail.com>
2025-05-17 13:57:19 +0800

    add docker login shell to build.sh

--- build.sh
+++ build.sh
@@ -2,16 +2,17 @@
 
 # check args
 if [ $# -lt 1 ]; then
-    echo "Error: Version argument is required."
-    echo "Usage: v$0 <version> [platform] (default platform: linux_amd64)"
-    exit 1
+  echo "Error: Version argument is required."
+  echo "Usage: v$0 <version> [platform] (default platform: linux_amd64)"
+  exit 1
 fi
 
 VERSION=$1
 PLATFORM=${2:-linux_amd64}
 PACKAGE_NAME=certimate.zip
-IMAGE_NAME_VERSIONED=registry.cn-shanghai.aliyuncs.com/maxwen/certimate-docker:${VERSION}
-IMAGE_NAME_LATEST=registry.cn-shanghai.aliyuncs.com/maxwen/certimate-docker:latest
+IMAGE_REPO=registry.cn-shanghai.aliyuncs.com
+IMAGE_NAME_VERSIONED=$IMAGE_REPO/maxwen/certimate-docker:${VERSION}
+IMAGE_NAME_LATEST=$IMAGE_REPO/maxwen/certimate-docker:latest
 
 DOWNLOAD_URL="https://github.com/usual2970/certimate/releases/download/v${VERSION}/certimate_${VERSION}_${PLATFORM}.zip"
 
@@ -20,8 +21,8 @@ mkdir -p ./package
 # download release package
 echo "Downloading certimate ${VERSION} for ${PLATFORM}..."
 if ! curl -L --fail --progress-bar -o ${PACKAGE_NAME} "${DOWNLOAD_URL}"; then
-    echo "Error: Failed to download package. Check version/platform availability."
-    exit 1
+  echo "Error: Failed to download package. Check version/platform availability."
+  exit 1
 fi
 
 # unpack
@@ -34,10 +35,15 @@ rm -f ${PACKAGE_NAME}
 
 echo "Successfully downloaded and extracted certimate ${VERSION} for ${PLATFORM}"
 
+echo "Enter docker registry login password:"
+
+# login to docker registry
+docker login $IMAGE_REPO --username=maxwen
+
 # build docker image
 docker build -t ${IMAGE_NAME_VERSIONED} .
 docker push ${IMAGE_NAME_VERSIONED}
 docker tag ${IMAGE_NAME_VERSIONED} ${IMAGE_NAME_LATEST}
 docker push ${IMAGE_NAME_LATEST}
 
-echo "Successfully built and pushed Docker image: \n ${IMAGE_NAME_VERSIONED}\n ${IMAGE_NAME_LATEST}"
+echo -e "Successfully built and pushed Docker image: \n ${IMAGE_NAME_VERSIONED}\n ${IMAGE_NAME_LATEST}"
